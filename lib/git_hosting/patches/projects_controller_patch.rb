38368a37925285aabfa5168c3d31c673ee92c05f
Pedro Algarvio <pedro@algarvio.me>
2011-07-02 23:15:28 +0100

    Don't ignore parent projects(more than one) hierarchy.
    This would happen when creating new repositories, but only on the
    RM/CP database side, on the gitolite side, all was done correctly.

--- lib/git_hosting/patches/projects_controller_patch.rb
+++ lib/git_hosting/patches/projects_controller_patch.rb
@@ -16,7 +16,8 @@ module GitHosting
 				end
 				if Setting.plugin_redmine_git_hosting['allProjectsUseGit'] == "true"
 					repo = Repository::Git.new
-					repo_name= @project.parent ? File.join(@project.parent.identifier,@project.identifier) : @project.identifier
+					repo_name = GitHosting.repository_name(@project)
+					#repo_name= @project.parent ? File.join(@project.parent.identifier,@project.identifier) : @project.identifier
 					repo.url = repo.root_url = File.join(Setting.plugin_redmine_git_hosting['gitRepositoryBasePath'], "#{repo_name}.git")
 					@project.repository = repo
 				end
